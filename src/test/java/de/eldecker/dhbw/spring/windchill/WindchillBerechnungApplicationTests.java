a31f70fe0bed7860355f6aabd4fcb8cc2a7678d3
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-11-12 16:00:04 +0100

    Projekt erzeugt von Spring Initializr
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.5.7&packaging=jar&configurationFileFormat=properties&jvmVersion=21&groupId=de.eldecker.dhbw.spring&artifactId=windchill&name=Windchill-Berechnung&description=Berechnung%20Windchill-Temperatur&packageName=de.eldecker.dhbw.spring.windchill&dependencies=web

---
+++ src/test/java/de/eldecker/dhbw/spring/windchill/WindchillBerechnungApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.windchill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindchillBerechnungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
